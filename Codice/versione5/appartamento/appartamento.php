f61d9a15f54e5b527bfff537f91f8fa5f249addb
Alessandro Spagnuolo <31542866+AlessandroSpagnuolo@users.noreply.github.com>
2018-05-11 16:16:28 +0200

    Add files via upload

--- Codice/versione5/appartamento/appartamento.php
+++ Codice/versione5/appartamento/appartamento.php
@@ -47,22 +47,32 @@
 			array_push($immagini, base64_encode($row[0]));
 		}
 	}
-	$sql = "select regione,n_locali,posteggio,ammobiliato,fumatori,animali,bambini from appartamento where id= '".$id."'";
+	$sql = "select regione,n_locali,posteggio,ammobiliato,fumatori,animali,bambini,commenti,ubicazione,username_prop from appartamento where id= '".$id."'";
 	$r = $conn->query($sql);
 	$prezzo;
 	$tipo;
 	$regione;
+	$commenti;
 	$n_locali;
 	$posteggio = "no";
 	$ammobiliato = "no";
 	$fumatori = "no";
 	$animali = "no";
 	$bambini = "no";
+	$indirizzo;
+	$u_prop;
+	$u_nome;
+	$u_cognome;
+	$u_cellulare;
+	$u_email;
 	if ($r->num_rows > 0) {
 	  //output data of each row
 		while($row = $r->fetch_assoc()) {
 			$regione = $row['regione'];
 			$n_locali = $row['n_locali'];
+			$commenti = $row['commenti'];
+			$indirizzo = $row['ubicazione'];
+			$u_prop = $row['username_prop'];
 			if($row['posteggio'] == 1)
 				$posteggio = "si";
 			if($row['ammobiliato'] == 1)
@@ -84,20 +94,61 @@
 			$tipo = $row['tipo'];
 		}
 	}
+	$piantina;
+	$sql = "select piantina from appartamento where id = '".$id."'";
+	$r = $conn->query($sql);
+	if ($r->num_rows > 0) {
+	  //output data of each row
+		while($row = $r->fetch_assoc()) {
+			$piantina = base64_encode($row['piantina']);
+		}
+	}
+	$sql = "select nome,cognome,n_cellulare,email from utente where username = '".$u_prop."'";
+	$r = $conn->query($sql);
+	if ($r->num_rows > 0) {
+	  //output data of each row
+		while($row = $r->fetch_assoc()) {
+			$u_nome = $row['nome'];
+			$u_cognome = $row['cognome'];
+			$u_cellulare = $row['n_cellulare'];
+			$u_email = $row['email'];
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Visualizzazione appartamento</title>
+		<style>
+			div{
+				border: solid 1px black;
+				border-radius: 5px;
+				background-color: rgb(240,240,240);
+				box-shadow: 0px 0px 10px 3px rgba(0,0,0,0.2);
+				padding: 10px;
+			}
+			td{
+				padding: 10px;
+			}
+			td img{
+				padding: 0px;
+				margin: 0px;
+				max-width: 400px;
+				max-height: 250px;
+				width: auto;
+				height: auto;
+			}
+		</style>
 	</head>
 	<body>
+		<table style="margin: auto;">
 		<h1><?php echo $titolo; ?></h1>
-		<table>
 			<tr>
 				<td><div id="immagini"></div></td>
 				<td><div id="info">
-					<p>Prezzo: <?php echo $prezzo; echo " ".$tipo ?></p>
+					<p>Prezzo: <?php echo $prezzo; echo ".- / ".$tipo ?></p>
 					<p>Regione: <?php echo $regione; ?></p>
+					<p>Indirizzo: <?php echo $indirizzo ?></p>
 					<p>N°locali: <?php echo $n_locali; ?></p>
 					<p>Posteggio: <?php echo $posteggio; ?></p>
 					<p>Ammobiliato: <?php echo $ammobiliato; ?></p>
@@ -106,12 +157,24 @@
 					<p>Bambini: <?php echo $bambini; ?></p>
 				</div></td>
 			</tr>
+			<tr>
+				<td><div>Piantina: <img src="data:image/png;base64,<?php echo $piantina; ?>"></div></td>
+				<td><div>Commenti: <br> <?php echo $commenti; ?></div></td>
+			</tr>
+			<tr>
+				<td><div id="contatto">Contatto: <?php echo $u_nome." ".$u_cognome; ?></div></td>
+				<td><div>
+					<p>N°Cellulare: <?php echo $u_cellulare; ?></p>
+					<p>E-Mail: <?php echo $u_email; ?></p>
+				</div></td>
+			</tr>
+		</table>
 		<script>
 			var immagini = ['<?php echo implode("','",$immagini);?>'];
 			var output = "";
 			for(var i = 0; i < immagini.length; i++){
 				if(immagini[i] != "") output += "<img src='data:image/png;base64,"+immagini[i]+"'>";
-				//else output = "<img src='../index/img/default_room.png'>";
+				else output = "<img src='../index/img/default_room.png'>";
 			}
 			document.getElementById('immagini').innerHTML = output;
 		</script>
